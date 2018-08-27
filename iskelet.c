97ad2ac038b44363b76d52837b9ac2722e18ed67
Alper Karakaya <malperkarakaya@gmail.com>
2018-08-27 09:40:03 +0300

    Create iskelet.c

---
+++ iskelet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Turkish");
+    
+    // Ana omurga
+    
+    getch();
+
+    return 0;
+}
